28c3212999ddb0bf1a634a9a0a2d368351af3c8f
Zhe Yu <zyu9@ncsu.edu>
2017-10-08 10:33:58 -0400

    lib

---
+++ prepare.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# chmod +x prepare.sh
+
+wget http://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh
+bash Miniconda2-latest-Linux-x86_64.sh
+
+conda install pip
+conda install scikit-learn
+conda install matplotlib
+
